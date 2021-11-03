ff5f9797ae07449fb0aee824e91119642c48ac70
Uliya Snisar <Uliya19191@gmail.com>
2021-11-03 23:51:35 +0200

    added packages files and changes to hw-04

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const boolParser = require('express-query-boolean')
+const helmet = require('helmet')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/contacts/contacts')
+const usersRouter = require('./routes/users/users')
+const { Limit } = require('./config/constants');
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: Limit.JSON}))
+app.use(boolParser())
 
+app.use((req, res, next) => {
+  app.set('lang', req.acceptsLanguages(['en', 'ru']))
+  next()
+})
+
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
@@ -19,9 +31,6 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  if (err.name === 'ValidationError') {
-    res.status(400).json({ status: 'error', code: 400, message: err.message })
-  }
   res.status(500).json({ status: 'fail', code: 500, message: err.message })
 })
 
--- config/constant.js
+++
@@ -1,8 +0,0 @@
-const ValidLengthOfContactName = {
-  MIN_LENGTH_OF_NAME: 3,
-  MAX_LENGTH_OF_NAME: 30,
-};
-
-module.exports = {
-  ValidLengthOfContactName,
-};
